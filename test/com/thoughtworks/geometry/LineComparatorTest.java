200e04f6f36b8520496776a5df74d06d6fb85402
thejasbabu <thejasb@thoughtworks.com>
2015-08-26 22:53:40 +0530

    Added a spec for comparing lines with same coordinates

--- test/com/thoughtworks/geometry/LineComparatorTest.java
+++ test/com/thoughtworks/geometry/LineComparatorTest.java
@@ -6,11 +6,21 @@ import org.junit.Test;
 public class LineComparatorTest {
 
     @Test
-    public void forLinesWithSameCoordinate0000(){
+    public void forLinesWithSameCoordinates0000(){
         Line lineOne = new Line(0,0,0,0);
         Line lineTwo = new Line(0,0,0,0);
         LineComparator comparator = new LineComparator(lineOne,lineTwo);
         Assert.assertEquals(true,comparator.compareTwoLines());
     }
+
+    @Test
+    public void forLinesWithSameXAndYCoordinates(){
+        Line lineOne = new Line(1,0,0,0);
+        Line lineTwo = new Line(1,0,0,0);
+        LineComparator comparator = new LineComparator(lineOne,lineTwo);
+        Assert.assertEquals(true, comparator.compareTwoLines());
+    }
+
+
 
 }
