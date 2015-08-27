3e5ba1463ecff30ce9fa21d755d73ecbc0219417
thejasbabu <thejasb@thoughtworks.com>
2015-08-27 11:44:26 +0530

    Changed the model,removed the class for comparing.

--- test/com/thoughtworks/geometry/LineTest.java
+++ test/com/thoughtworks/geometry/LineTest.java
@@ -3,29 +3,28 @@ package com.thoughtworks.geometry;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class LineTest {
 
     @Test
-    public void linewithstartingindex00andendingindex00(){
+    public void lineWithStartingIndex00AndEndingIndex00(){
         Line line = new Line(0,0,0,0);
         Assert.assertEquals(0.0f, line.length(),0.0);
     }
 
     @Test
-    public void linewithstartingindex00andendingindex01() {
+    public void lineWithStartingIndex00AndEndingIndex01() {
         Line line = new Line(0,0,0,1);
         Assert.assertEquals(1.0f, line.length(),0.0);
     }
 
     @Test
-    public void linewithstartingindex12andendingindex35() {
+    public void lineWithStartingIndex12AndEndingIndex35() {
         Line line = new Line(1,2,3,5);
         Assert.assertEquals(3.6, line.length(),0.05);
     }
 
     @Test
-    public void linewithnegativeindex(){
+    public void lineWithNegativeIndex(){
         Line line= new Line(0,0,-1,0);
         Assert.assertEquals(1.0, line.length(),0.0);
     }
